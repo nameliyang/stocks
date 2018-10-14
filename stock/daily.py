18269982c4e37c9d789eeef8cbbd5a7e4b1b26ab
nameliyang <1289714862@qq.com>
2018-10-14 21:57:33 +0800

    init

--- stock/daily.py
+++ stock/daily.py
@@ -18,11 +18,39 @@ def get_k_data(code, start, end):
     return df.to_json(orient='records')
 '''
 
+
 def get_k_data(code, start, end):
     df = ts.get_k_data(code, start=start, end=end)
+    df = getMACD(df)
     return df.to_json(orient='records')
 
 
-if(__name__=='__main__'):
-    data = get_k_data('000970','2018-01-01','2018-09-01')
+def get_EMA(df, N):
+    for i in range(len(df)):
+        if i == 0:
+            df.ix[i, 'ema'] = df.ix[i, 'close']
+        if i > 0:
+            df.ix[i, 'ema'] = (2 * df.ix[i, 'close'] + (N - 1) * df.ix[i - 1, 'ema']) / (N + 1)
+    ema = list(df['ema'])
+    return ema
+
+
+def getMACD(df, short=12, long=26, M=9):
+    if len(df) == 0:
+        return df
+    a = get_EMA(df, short)
+    b = get_EMA(df, long)
+    df['diff'] = pd.Series(a) - pd.Series(b)
+    # print(df['diff'])
+    for i in range(len(df)):
+        if i == 0:
+            df.ix[i, 'dea'] = df.ix[i, 'diff']
+        if i > 0:
+            df.ix[i, 'dea'] = (2 * df.ix[i, 'diff'] + (M - 1) * df.ix[i - 1, 'dea']) / (M + 1)
+    df['macd'] = 2 * (df['diff'] - df['dea'])
+    return df
+
+
+if (__name__ == '__main__'):
+    data = get_k_data('000970', '2018-01-01', '2018-09-01')
     print(data)
