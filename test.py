68d7d3376e459752d74e4c854413b1fa79d5b66a
Li Yang <yli587@best-inc.com>
2018-10-18 18:36:26 +0800

    no message

--- test.py
+++ test.py
@@ -8,23 +8,18 @@ second_macd = None
 third_macd = None
 isBuy = False
 pre_macd = None;
-for row in df.iterrows():
+
+for index, row in df.iterrows():
     date = row['date']
     macd = row['macd']
-
-
-    if not first_macd and macd < 0:
+    open = row['open']
+    close = row['close']
+    low = row['low']
+    high = row['high']
+    # print(str(open)+'\t'+str(close)+'\t'+str(low)+'\t'+str(high)+'\t'+str(macd)+'\t'+ date)
+    if not first_macd and not second_macd and not third_macd:
         first_macd = macd
-
-    if first_macd and not second_macd and macd < 0:
+    elif first_macd and not second_macd and not third_macd:
         second_macd = macd
-    else:
-        first_macd = second_macd
-        second_macd = None
-
-    if first_macd and second_macd and (second_macd-first_macd < macd - second_macd):
-        isBuy = True
-    else:
-        first_macd = None
-        second_macd = None
-        third_macd = None
+    elif first_macd and  second_macd and not third_macd:
+        third_macd = macd
