528eaa67cfff454361e1cdd16abb5beabf374c10
nameliyang <1289714862@qq.com>
2018-10-24 21:39:40 +0800

    no message

--- talib-macd-matplotlib-example.py
+++ talib-macd-matplotlib-example.py
@@ -1,16 +1,15 @@
 import matplotlib.pyplot as plt
+import mpl_finance as mpf
 import numpy as np
 import pandas as pd
 import talib as ta
-from matplotlib.dates import date2num
-from mpl_finance import candlestick_ohlc as candlestick
 from pandas_datareader import data
 
+pd.set_option('display.max_columns', 500)
 ticker = 'OPK'
 
 # Download sample data
 sec_id = data.get_data_robinhood(ticker, '2018-06-01')
-print(sec_id.index)
 # Data for matplotlib finance plot
 # sec_id_ochl = np.array(pd.DataFrame({'0':date2num(sec_id.index.to_pydatetime()),
 #                                   '1':sec_id.Open,
@@ -31,51 +30,63 @@ STOCH_D = 3
 SIGNAL_TOL = 3
 Y_AXIS_SIZE = 12
 
-analysis = pd.DataFrame(index = sec_id.index)
-
+analysis = pd.DataFrame(index=sec_id.index)
+sec_id.rename(columns={'close_price': 'Close', 'high_price': 'High', 'low_price': 'Low', 'open_price': 'Open'},
+              inplace=True)
+print(sec_id)
 # analysis['sma_f'] = pd.rolling_mean(sec_id.Close, SMA_FAST)
 # analysis['sma_s'] = pd.rolling_mean(sec_id.Close, SMA_SLOW)
 
 analysis['sma_f'] = sec_id.Close.rolling(SMA_FAST).mean()
 analysis['sma_s'] = sec_id.Close.rolling(SMA_SLOW).mean()
 
-analysis['rsi'] = ta.RSI(sec_id.Close.as_matrix(), RSI_PERIOD)
-analysis['sma_r'] = pd.rolling_mean(analysis.rsi, RSI_AVG_PERIOD) # check shift
-analysis['macd'], analysis['macdSignal'], analysis['macdHist'] = ta.MACD(sec_id.Close.as_matrix(), fastperiod=MACD_FAST, slowperiod=MACD_SLOW, signalperiod=MACD_SIGNAL)
-analysis['stoch_k'], analysis['stoch_d'] = ta.STOCH(sec_id.High.as_matrix(), sec_id.Low.as_matrix(), sec_id.Close.as_matrix(), slowk_period=STOCH_K, slowd_period=STOCH_D)
+close_real_data = np.array(sec_id.Close.values, dtype='f8')
+analysis['rsi'] = ta.RSI(close_real_data, RSI_PERIOD)
+# analysis['sma_r'] = pd.rolling_mean(analysis.rsi, RSI_AVG_PERIOD)
+analysis['sma_r'] = analysis.rsi.rolling(RSI_AVG_PERIOD).mean()
+# check shift
+
+analysis['macd'], analysis['macdSignal'], analysis['macdHist'] = ta.MACD(close_real_data, fastperiod=MACD_FAST,
+                                                                         slowperiod=MACD_SLOW, signalperiod=MACD_SIGNAL)
+high_real_data = np.array(sec_id.High.values, dtype='f8')
+low_real_data = np.array(sec_id.Low.values, dtype='f8')
+analysis['stoch_k'], analysis['stoch_d'] = ta.STOCH(high_real_data, low_real_data,
+                                                    close_real_data, slowk_period=STOCH_K,
+                                                    slowd_period=STOCH_D)
 
 analysis['sma'] = np.where(analysis.sma_f > analysis.sma_s, 1, 0)
 analysis['macd_test'] = np.where((analysis.macd > analysis.macdSignal), 1, 0)
 analysis['stoch_k_test'] = np.where((analysis.stoch_k < 50) & (analysis.stoch_k > analysis.stoch_k.shift(1)), 1, 0)
 analysis['rsi_test'] = np.where((analysis.rsi < 50) & (analysis.rsi > analysis.rsi.shift(1)), 1, 0)
-
+fig = plt.figure(figsize=(32, 16))
 # Prepare plot
 fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True)
 ax1.set_ylabel(ticker, size=20)
 
-#size plot
-fig.set_size_inches(15,30)
+# size plot
+fig.set_size_inches(15, 30)
 
 # Plot candles
-candlestick(ax1, sec_id_ochl, width=0.5, colorup='g', colordown='r', alpha=1)
-
+#candlestick(ax1, sec_id_ochl, width=0.5, colorup='g', colordown='r', alpha=1)
+mpf.candlestick2_ochl(ax1, sec_id['Open'], sec_id['Close'], sec_id['High'], sec_id['Low'], width=0.5, colorup='r',
+                      colordown='green', alpha=0.6)
 # Draw Moving Averages
 analysis.sma_f.plot(ax=ax1, c='r')
 analysis.sma_s.plot(ax=ax1, c='g')
 
-#RSI
+# RSI
 ax2.set_ylabel('RSI', size=Y_AXIS_SIZE)
-analysis.rsi.plot(ax = ax2, c='g', label = 'Period: ' + str(RSI_PERIOD))
-analysis.sma_r.plot(ax = ax2, c='r', label = 'MA: ' + str(RSI_AVG_PERIOD))
+analysis.rsi.plot(ax=ax2, c='g', label='Period: ' + str(RSI_PERIOD))
+analysis.sma_r.plot(ax=ax2, c='r', label='MA: ' + str(RSI_AVG_PERIOD))
 ax2.axhline(y=30, c='b')
 ax2.axhline(y=50, c='black')
 ax2.axhline(y=70, c='b')
-ax2.set_ylim([0,100])
+ax2.set_ylim([0, 100])
 handles, labels = ax2.get_legend_handles_labels()
 ax2.legend(handles, labels)
 
 # Draw MACD computed with Talib
-ax3.set_ylabel('MACD: '+ str(MACD_FAST) + ', ' + str(MACD_SLOW) + ', ' + str(MACD_SIGNAL), size=Y_AXIS_SIZE)
+ax3.set_ylabel('MACD: ' + str(MACD_FAST) + ', ' + str(MACD_SLOW) + ', ' + str(MACD_SIGNAL), size=Y_AXIS_SIZE)
 analysis.macd.plot(ax=ax3, color='b', label='Macd')
 analysis.macdSignal.plot(ax=ax3, color='g', label='Signal')
 analysis.macdHist.plot(ax=ax3, color='r', label='Hist')
@@ -85,8 +96,8 @@ ax3.legend(handles, labels)
 
 # Stochastic plot
 ax4.set_ylabel('Stoch (k,d)', size=Y_AXIS_SIZE)
-analysis.stoch_k.plot(ax=ax4, label='stoch_k:'+ str(STOCH_K), color='r')
-analysis.stoch_d.plot(ax=ax4, label='stoch_d:'+ str(STOCH_D), color='g')
+analysis.stoch_k.plot(ax=ax4, label='stoch_k:' + str(STOCH_K), color='r')
+analysis.stoch_d.plot(ax=ax4, label='stoch_d:' + str(STOCH_D), color='g')
 handles, labels = ax4.get_legend_handles_labels()
 ax4.legend(handles, labels)
 ax4.axhline(y=20, c='b')
